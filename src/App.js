93d59bc38042c5f8e521d1e91e7d0c5651532511
tommynam1994 <tommynam1994@gmail.com>
2021-08-30 18:49:09 +0800

    implemented redux, need to create database

--- src/App.js
+++ src/App.js
@@ -1,73 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Posts from "./components/Posts"
-import { createStore } from "redux";
+import PostForm from "./components/PostForm"
+
+import { applyMiddleware, createStore } from "redux";
 import { Provider, connect } from "react-redux";
 
-
-//INITIALIZE INITIAL STATE
-const initialState = {
-  linksReducer: []
-};
-
-//CREATE PURELINK LIST VARIABLE 
-const PureLinkList = (props) => {
-  return (
-    <div>
-      <button onClick={props.addLinkMDP}>New Link</button> 
-      <button onClick={props.clearLinkMDP}>Clear</button> 
-      {props.linksMSP.map(l => (
-        <div>{l.title} - {l.url}</div>
-      ))}
-    </div>
-  );
-}
-
-//ROOT REDUCER
-const rootReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case ADD_LINK_ACTION: {
-      return linksReducer.state.linksReducer.concat([action.link])
-      }
-    case CLEAR_LINK_ACTION: {
-      return {
-        linksReducer: []
-      }
-      }
-    default: {
-      return state;
-      }
-  }
- };
-
-//INSTANTIATE STORE VARIABLE - USE CREATESTORE
-const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
-
-//MAP STATE TO PROPS - CONNECT LINKLIST TO REDUX STORE
-const mapStateToProps = (state) => {
-  return {
-    linksMSP: state.linksRootReducer
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addLinkMDP: () => dispatch({
-      type: ADD_LINK_ACTION,
-      link: {
-        title: 'Xccelerate',    
-        url: 'https://www.xccelerate.co'
-      }
-    }),
-    clearLinkMDP: () => dispatch({
-      type: CLEAR_LINK_ACTION
-    })
-  }
-}
-
-
-const LinkList = connect(mapStateToProps, mapDispatchToProps)(PureLinkList)
-
+import store from "./redux/store";
 
 
 //APP
@@ -77,6 +16,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <PostForm />
+          <hr />
           <Posts />
         </header>
       </div>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPosts } from "../redux/actions/postActions";
 
 class Posts extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.newPost) {
+            this.props.posts.unshift(nextProps.newPost);
+        }
+    }
+
     render() {
+        const postItems = this.props.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p> {post.body}</p>
+            </div>
+        ));
         return (
             <div>
-                <p>Posts</p>
+                {postItems}
             </div>
         )
     }
 }
 
-export default Posts;
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+    posts: state.posts.items,
+    newPost: state.posts.item,
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
